b331cc12ea82c2ea312729de01e50e51d49cf297
Artem Nistuley <artem.nistuley@gmail.com>
2025-07-27 01:38:46 +0300

    add starlight-theme-nova

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,40 +1,46 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeNova from 'starlight-theme-nova'
 
 const sidebar = [
-	{
-		label: 'Getting Started',
-		collapsed: true,
-		autogenerate: { directory: 'getting-started' },
-	},
-	{
-		label: 'Guides',
-		collapsed: true,
-    autogenerate: { directory: 'guides', collapsed: true },
-	},
   {
-    label: 'References',
+    label: 'Getting Started',
     collapsed: true,
-    autogenerate: { directory: 'references', collapsed: true },
+    autogenerate: { directory: 'getting-started' },
+  },
+  {
+    label: 'Guides',
+    collapsed: true,
+    autogenerate: { directory: 'guides', collapsed: true },
   },
 ];
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Kameo',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/kameojs/kameo' }],
-			sidebar,
-			components: {
-				Hero: './src/components/overrides/Hero.astro',
-				ThemeSelect: './src/components/overrides/ThemeSelect.astro',
-				ThemeProvider: './src/components/overrides/ThemeProvider.astro',
-			},
-			customCss: [
-				'./src/styles/main.css',
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'Kameo',
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/kameojs/kameo' }],
+      sidebar,
+      components: {
+        Hero: './src/components/overrides/Hero.astro',
+        ThemeSelect: './src/components/overrides/ThemeSelect.astro',
+        ThemeProvider: './src/components/overrides/ThemeProvider.astro',
+      },
+      customCss: [
+        './src/styles/main.css',
+      ],
+      plugins: [
+        starlightThemeNova({
+          nav: [
+            {
+              label: 'Docs',
+              href: '/getting-started/introduction',
+            },
+          ],
+        }),
+      ],
+    }),
+  ],
 });
